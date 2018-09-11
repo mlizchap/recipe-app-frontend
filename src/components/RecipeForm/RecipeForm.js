834d8473287d2298d710b14cebe8589f66bc829e
mlizchap <mlizchap@gmail.com>
2018-09-11 17:32:11 -0400

    refactored form page

--- src/components/RecipeForm/RecipeForm.js
+++ src/components/RecipeForm/RecipeForm.js
@@ -5,39 +5,53 @@ class RecipeForm extends Component {
         super(props);
 
         this.state = {
-            userInput: {
-                title: 'this',
-                image: 'fasfsdfas',
-                ingredients: [{name: 'xxx', amount: '2 cups'}, {name: 'fdasf', amount: '435'}],
-                description: 'that',
-            }
+            titleInput: 'this',
+            imageInput: 'fasfsdfas',
+            ingredientInput: [{name: 'xxx', amount: '2 cups'}, {name: 'fdasf', amount: '435'}],
+            descriptionInput: 'that',
         }
     }
-    handleChange = (e, key) => {
-        console.log(key)
-        console.log(this.state.userInput[key])
-        const userInput = {...this.state.userInput, [key]: e.target.value}
-        this.setState({userInput})
+    handleChange = (e) => {
+        this.setState({ [e.target.name]: e.target.value})
     }
     render() {
         return (
             <form>
-                {Object.keys(this.state.userInput).map(key => {
-                    var item = this.state.userInput[key];
-                    return (Array.isArray(item)) ? 
-                        item.map((i, index) => {
-                            return (
-                                <div key={i.name}>
-                                    {(index === 0) ? <div>ingredients</div> : null}
-                                    <input value={i.name} />
-                                    <input value={i.amount} />
-                                </div>
-                            )
-                        }) : <div>{key}<input value={item} onChange={(e) => this.handleChange(e, key)} /></div>
-                })}
+                <div>title<input name="titleInput" value={this.state.titleInput} onChange={this.handleChange} /></div>
+                <div>image<input name="imageInput" value={this.state.imageInput} onChange={this.handleChange} /></div>
+
+                <div>description<input name="descriptionInput" value={this.state.descriptionInput} onChange={this.handleChange} /></div>
             </form>
-        );
+        )
     }
+    // handleChange = (e, key) => {
+    //     const userInput = {...this.state.userInput, [key]: e.target.value}
+    //     this.setState({userInput})
+    // }
+    // handleIngredChange = (e,key,index) => {
+    //     const userInput = {...this.state.userInput}
+    //     userInput[key][index] = { ...userInput[key][index],[e.target.name]: e.target.value }
+    //     this.setState({userInput})
+    // }
+    // render() {
+    //     return (
+    //         <form>
+    //             {Object.keys(this.state.userInput).map(key => {
+    //                 var item = this.state.userInput[key];
+    //                 return (Array.isArray(item)) ? 
+    //                     item.map((i, index) => {
+    //                         return (
+    //                             <div key={i.name}>
+    //                                 {(index === 0) ? <div>ingredients</div> : null}
+    //                                 <input value={i.name} name="name" onChange={(e) => this.handleIngredChange(e, key, index)} />
+    //                                 <input value={i.amount} name="amount" onChange={(e) => this.handleIngredChange(e, key, index)} />
+    //                             </div>
+    //                         )
+    //                     }) : <div key={key}>{key}<input value={item} onChange={(e) => this.handleChange(e, key)} /></div>
+    //             })}
+    //         </form>
+    //     );
+    // }
 }
 
 export default RecipeForm;

bdf43ece52bd66d5ad3747abbfa269180a1c8ec1
mlizchap <mlizchap@gmail.com>
2018-09-13 18:27:53 -0400

    can submit new recipe and DB updates

--- src/components/DataProvider/DataProvider.js
+++ src/components/DataProvider/DataProvider.js
@@ -3,24 +3,7 @@ import axios from 'axios';
 import _ from 'lodash';
 
 export const DataContext = React.createContext();
-
-const test = {
-    1: {
-        _id: 1,
-        title: 'brownies',
-        image: 'fasfsdfas',
-        ingredients: [{name: 'xxx', amount: '2 cups'}, {name: 'fdasf', amount: '435'}],
-        description: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?',
-
-    },
-    2: {
-        _id: 2,
-        title: 'cake',
-        image: 'fasfsdfas',
-        ingredients: [{name: 'zzz', amount: '77 cups'}, {name: 'yyy', amount: '4000'}],
-        description: 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.',
-    }
-}
+const baseURL = 'https://arcane-lowlands-94627.herokuapp.com/api';
 
 class DataProvider extends Component {
     constructor(props) {
@@ -30,20 +13,17 @@ class DataProvider extends Component {
          }
     }
     componentDidMount() {
-        //get state from DB 
-        axios.get('https://arcane-lowlands-94627.herokuapp.com/api')
+        axios.get(`${baseURL}`)
             .then(res => {
-                console.log(res.data)
                 const recipeHashMap = _.mapKeys(res.data, '_id')
                 this.setState({ recipes: recipeHashMap })
             })
-
-        // concvert data from 
-
-        //this.setState({ recipes: })
     }
     // function for adding data
-
+    addData = (e, values) => {
+        e.preventDefault();
+        axios.post(`${baseURL}/new`, values)
+    }
     // function for deleting data
 
     // function for editing data
@@ -52,12 +32,7 @@ class DataProvider extends Component {
         return (
             <DataContext.Provider value={{
                 state: this.state,
-                handleSubmit: (e, values) => {
-                    e.preventDefault()
-                    for (let i in values) {
-                        console.log(values[i])
-                    }
-                }
+                handleSubmit: this.addData
             }}>
                 {this.props.children}
             </DataContext.Provider>
